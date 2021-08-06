af09db528bcdd9a9c4d415ca9779c910831f31d4
Arturo <45513197+Calisto184AC0@users.noreply.github.com>
2021-08-06 08:22:05 +0200

    [html] comienzo home

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,5 @@ let openMenuIcon = document.getElementById('irf-open-menu')
 let closeMenuIcon = document.getElementById('irf-close-menu')
 let menuElement = document.getElementsByClassName('irf-nav')[0]
 
-openMenuIcon.onclick = () => {
-    menuElement.style.transform = 'translate(0%)'
-}
-
-closeMenuIcon.onclick = () => {
-    menuElement.style.transform = ''
-}
+openMenuIcon.onclick = () => { menuElement.style.transform = 'translate(0%)' }
+closeMenuIcon.onclick = () => { menuElement.style.transform = '' }
